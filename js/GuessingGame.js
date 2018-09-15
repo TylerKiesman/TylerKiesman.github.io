5092c2e74c6d6e1199afe685a78b3adf622c0b25
Tyler Kiesman <tylerkiesman@gmail.com>
2018-09-15 12:47:42 -0400

    Added in jQuery

--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -1,27 +1,27 @@
 var correctAns;
 resetAns();
-var response = document.getElementById("guess");
+var $response = $("#guess");
 var originalTxt = "Type in a number and press enter to guess!";
-var hintSaying = document.getElementById("hint");
-var rbtn = document.getElementById("resetBTN");
-rbtn.style.visibility = 'hidden';
+var $hintSaying = $("#hint");
+var $rbtn = $("#resetBTN");
+$rbtn.hide();
 
 
 //Checks the users guess
 function checkAns() {
-	if(response.value == correctAns) {
-		hintSaying.textContent = 'Congrats you got it!';
-		rbtn.style.visibility = 'visible';
-	} else if(response.value < correctAns) {
-		hintSaying.textContent = "Maybe try a little bit higher than " + response.value.toString() + "...";
-		response.value = '';
-	} else if(response.value > correctAns){
-		hintSaying.textContent = "Maybe try a little bit lower than " + response.value.toString() + "...";
-		response.value = '';
+	if($response.val() == correctAns) {
+		$hintSaying.text('Congrats you got it!');
+		$rbtn.show();
+	} else if($response.val() < correctAns) {
+		$hintSaying.text( "Maybe try a little bit higher than " + $response.val().toString() + "...");
+		$response.val('');
+	} else if($response.val() > correctAns){
+		$hintSaying.text("Maybe try a little bit lower than " + $response.val().toString() + "...");
+		$response.val('');
 	}
 	else{
-		hintSaying.textContent = "Please enter a valid number...";
-		response.value = '';
+		$hintSaying.text("Please enter a valid number...");
+		$response.val('');
 	}
 }
 
@@ -31,16 +31,16 @@ function resetAns() {
 }
 
 //When the user hits enter the function to check the number is called
-response.addEventListener("keyup", function(event) {
+$response.keyup(function(event) {
 	if(event.keyCode === 13) {
 		checkAns();
 	}
 });
 
 //When the user clicks the reset button the game resets
-rbtn.addEventListener("click", function() {
+$rbtn.click(function() {
 	resetAns();
-	rbtn.style.visibility = 'hidden';
-	response.value = '';
-	hintSaying.textContent = originalTxt;
+	$rbtn.hide();
+	$response.val('');
+	$hintSaying.text(originalTxt);
 });

faf15bd05b448316908ead25b9d7a6e69624f8e6
Tyler Kiesman <tylerkiesman@gmail.com>
2019-01-31 15:38:06 -0500

    Should fix navbar issues on mobile devices

---
+++ js/setupSmallScreen.js
@@ -0,0 +1,4 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+  var elem = $('#ProjAndCont');
+  elem.remove();
+}
